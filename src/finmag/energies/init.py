91d9f92fe3a9b4f17e372b0d1a1d81d36d3c1543
davidcortesortuno <david.cortes@usm.cl>
2024-03-11 10:53:45 -0300

    Added cyl anisotropy to init

--- src/finmag/energies/init.py
+++ src/finmag/energies/init.py
@@ -3,6 +3,7 @@ from demag import Demag, Demag2D, MacroGeometry
 from energy_base import EnergyBase
 from exchange import Exchange
 from anisotropy import UniaxialAnisotropy
+from anisotropy_cyl import UniaxialAnisotropyCylindrical
 from cubic_anisotropy import CubicAnisotropy
 from zeeman import Zeeman, TimeZeeman, DiscreteTimeZeeman, OscillatingZeeman, TimeZeemanPython
 from dmi import DMI, DMI_interfacial
